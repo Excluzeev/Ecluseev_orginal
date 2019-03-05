a0064eea2013c46d544d983b0b7f13ef96585502
VJ Pranay <pranaymadasi1@gmail.com>
2019-03-05 16:41:05 +0530

    Added toolbar, Featured Image , Menu bar

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,25 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
+    <EToolBar />
     <v-content>
-      <HelloWorld/>
+      <div style="padding-top:25px"></div>
+      <FeaturedItem />
+      <HomeMenu />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import FeaturedItem from "./components/FeaturedItem";
+import EToolBar from "./components/EToolBar";
+import HomeMenu from "./components/HomeMenu";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
+    FeaturedItem,
+    EToolBar,
+    HomeMenu
   }
-}
+};
 </script>
