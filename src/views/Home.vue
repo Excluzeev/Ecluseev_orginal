4220788747ac91552940a55a4b8f6ad69e2383b8
Karthik <ponnamkarthik3@gmail.com>
2019-03-05 17:12:52 +0530

    removed helloworld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,19 +9,14 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "../components/HelloWorld";
-
 import FeaturedItem from "../components/FeaturedItem";
 import HomeMenu from "../components/HomeMenu";
-
 import CategoriesTrailerVideos from "../components/CategoriesTrailerVideos";
 
 export default {
   name: "home",
   components: {
-    HelloWorld,
     FeaturedItem,
-
     HomeMenu,
     CategoriesTrailerVideos
   }
