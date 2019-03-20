1445e4146f4e5dc1ce4e7ae539e5b06ffb0ed9a4
Karthik <ponnamkarthik3@gmail.com>
2019-03-20 23:48:56 +0530

    logo to trailer logo, Videos Page Done

---
+++ src/components/VideoDetailVideoItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <router-link
+          class="" :to="'/video/' + video.videoId">
+    <v-layout class="all-bg padding-top" justify-left row>
+      <img :src="video.image" height="94" width="168" />
+      <div class="padding">
+        <div class="title--text max-2-lines">{{ video.title }}</div>
+        <span class="desc--text">{{ video.channelName }}</span>
+        <div class="desc--text">
+          {{ video.views }} views • {{ video.timeAgo }}
+        </div>
+      </div>
+    </v-layout>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "VideoDetailVideoItem",
+  props: ["video"]
+};
+</script>
+
+<style scoped>
+  a {
+    text-decoration: none;
+  }
+  .padding {
+    padding: 10px;
+  }
+  .title--text {
+    color: #0a0a0a;
+    font-size: 1rem;
+    font-weight: 600;
+  }
+  .desc--text {
+    font-size: 0.95rem;
+  }
+  .padding-top {
+    padding-top: 10px;
+  }
+</style>
