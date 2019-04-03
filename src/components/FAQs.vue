a58f8436e35fd289d0966f9885ea5371d0f32a68
Karthik <ponnamkarthik3@gmail.com>
2019-04-03 22:52:25 +0530

    Some Fixes

--- src/components/FAQs.vue
+++ src/components/FAQs.vue
@@ -1,39 +1,45 @@
 <template>
-  <body>
+  <v-container>
     <div class="license_page">
-      <h1 class="main_text">FAQ's </h1>
-<p>1. What is Excluzeev?</p>
-<p>Excluzeev is a Streaming, Crowdfunding and Social Media Website/Mobile Application. Excluzeev offers content creators a platform to monetize their content.</p>
-<p>&nbsp;</p>
-<p>2. What is the payment gateway used on Excluzeev?</p>
-<p>Paypal is used as the payment gateway on Excluzeev.</p>
-<p>&nbsp;</p>
-<p>3. What is the best way to contact Excluzeev?</p>
-<p>Email us at <a href="mailto:support@excluzeev.com">support@excluzeev.com </a></p>
-<p>&nbsp;</p>
-<p>4. Are there age restrictions for being a content creator on Excluzeev?</p>
-<p>Content creators must be 18 years of age.</p>
-<p>&nbsp;</p>
-<p>5. How do I sign up as a user/content creator on Excluzeev?</p>
-<p>To sign up as a content creator you need to go to the sign up link on Excluzeev, add your name, email and password. Confirm your email. You create a channel by selecting a content category and adding your PayPal email for subscriber payments.</p>
-<p>&nbsp;</p>
-<p>6. Can a subscriber unsubscribe at any time with no commitment?</p>
-<p>Yes, you can unsubscribe at any time.</p>
-<p>&nbsp;</p>
-<p>7. Are people able to download or copy my content from the site?</p>
-<p>No they cannot. Your content will be stream only.</p>
-<p>&nbsp;</p>
-<p>8. How do I report content I find offensive or inappropriate?</p>
-<p>You can email <a href="mailto:support@excluzeev.com">support@excluzeev.com </a></p>
-<p>&nbsp;</p>
-<p>9. Is the a minimum or maximum price I am required to use when charging on my channel or can I charge anything I want?</p>
-<p>You cannot charge less than $1 and more than $10 per channel.</p>
-<p>&nbsp;</p>
-<p>10. Do you have to Subscribe to watch the Trailers?</p>
-<p>All trailers can be viewed without subscription.</p>"main_text">Licensing Agreement</h1>
-     
-       </div>
-  </body>
+      <h1 class="main_text">FAQ's</h1>
+      <p>1. What is Excluzeev?</p>
+      <p>Excluzeev is a Streaming, Crowdfunding and Social Media Website/Mobile Application. Excluzeev offers content creators a platform to monetize their content.</p>
+      <p>&nbsp;</p>
+      <p>2. What is the payment gateway used on Excluzeev?</p>
+      <p>Paypal is used as the payment gateway on Excluzeev.</p>
+      <p>&nbsp;</p>
+      <p>3. What is the best way to contact Excluzeev?</p>
+      <p>
+        Email us at
+        <a href="mailto:support@excluzeev.com">support@excluzeev.com</a>
+      </p>
+      <p>&nbsp;</p>
+      <p>4. Are there age restrictions for being a content creator on Excluzeev?</p>
+      <p>Content creators must be 18 years of age.</p>
+      <p>&nbsp;</p>
+      <p>5. How do I sign up as a user/content creator on Excluzeev?</p>
+      <p>To sign up as a content creator you need to go to the sign up link on Excluzeev, add your name, email and password. Confirm your email. You create a channel by selecting a content category and adding your PayPal email for subscriber payments.</p>
+      <p>&nbsp;</p>
+      <p>6. Can a subscriber unsubscribe at any time with no commitment?</p>
+      <p>Yes, you can unsubscribe at any time.</p>
+      <p>&nbsp;</p>
+      <p>7. Are people able to download or copy my content from the site?</p>
+      <p>No they cannot. Your content will be stream only.</p>
+      <p>&nbsp;</p>
+      <p>8. How do I report content I find offensive or inappropriate?</p>
+      <p>
+        You can email
+        <a href="mailto:support@excluzeev.com">support@excluzeev.com</a>
+      </p>
+      <p>&nbsp;</p>
+      <p>9. Is the a minimum or maximum price I am required to use when charging on my channel or can I charge anything I want?</p>
+      <p>You cannot charge less than $1 and more than $10 per channel.</p>
+      <p>&nbsp;</p>
+      <p>10. Do you have to Subscribe to watch the Trailers?</p>
+      <p>All trailers can be viewed without subscription.</p>
+      <!-- <h1 class="main_text">Licensing Agreement</h1> -->
+    </div>
+  </v-container>
 </template>
 
 <script>
@@ -51,5 +57,8 @@ export default {
 }
 .main_text {
   text-align: center;
+}
+p {
+  font-size: 16px;
 }
 </style>
