b3a01bc429885f20eff00373694c570036177960
Karthik Ponnam <ponnamkarthik3@gmail.com>
2019-06-03 07:35:14 +0530

    call to action in menu added

---
+++ src/views/SingleCategory.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container>
+    <div v-if="categories.length > 0">
+      <div v-for="category in categories" v-bind:key="category.id">
+        <category-trailers :catName="category"/>
+      </div>
+    </div>
+    <v-container v-else style="padding: 20px;">
+      <p class="text-xs-center" style="font-size: 20px;">Now Loading..</p>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+import CategoryTrailers from "../components/CategoryTrailers";
+
+export default {
+  name: "SingleCategory",
+  data: () => {
+    return {
+      categories: []
+    };
+  },
+  components: {
+    CategoryTrailers
+  },
+  created() {
+    let cat = this.$route.query.category;
+    console.log(cat);
+    if (cat.includes(",")) {
+      cat = cat.split(",");
+
+      this.categories = cat;
+    } else {
+      this.categories.push(cat);
+    }
+    console.log(this.categories);
+    // this.fetchCategoryTrailers("VGf1Xq2czICWnIHdg6Pz", "Comedy");
+  }
+};
+</script>
+
+<style scoped>
+.padding {
+  padding-left: 20px;
+  padding-right: 20px;
+}
+</style>
