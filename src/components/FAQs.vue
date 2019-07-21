c8df182cf7b651c9b3731ee3746a24e26acb09b2
VJ Pranay <pranaymadasi1@gmail.com>
2019-07-21 13:59:31 +0530

    Update FAQs.vue

--- src/components/FAQs.vue
+++ src/components/FAQs.vue
@@ -2,41 +2,35 @@
   <v-container>
     <div>
       <h1 class="main_text">FAQ's</h1>
-      <p>1. What is Excluzeev?</p>
-      <p>Excluzeev is a Streaming, Crowdfunding and Social Media Website/Mobile Application. Excluzeev offers content creators a platform to monetize their content.</p>
-      <p>&nbsp;</p>
-      <p>2. What is the payment gateway used on Excluzeev?</p>
-      <p>Paypal is used as the payment gateway on Excluzeev.</p>
-      <p>&nbsp;</p>
-      <p>3. What is the best way to contact Excluzeev?</p>
-      <p>
-        Email us at
-        <a href="mailto:support@excluzeev.com">support@excluzeev.com</a>
-      </p>
-      <p>&nbsp;</p>
-      <p>4. Are there age restrictions for being a content creator on Excluzeev?</p>
-      <p>Content creators must be 18 years of age.</p>
-      <p>&nbsp;</p>
-      <p>5. How do I sign up as a user/content creator on Excluzeev?</p>
-      <p>To sign up as a content creator you need to go to the sign up link on Excluzeev, add your name, email and password. Confirm your email. You create a channel by selecting a content category and adding your PayPal email for subscriber payments.</p>
-      <p>&nbsp;</p>
-      <p>6. Can a subscriber unsubscribe at any time with no commitment?</p>
-      <p>Yes, you can unsubscribe at any time.</p>
-      <p>&nbsp;</p>
-      <p>7. Are people able to download or copy my content from the site?</p>
-      <p>No they cannot. Your content will be stream only.</p>
-      <p>&nbsp;</p>
-      <p>8. How do I report content I find offensive or inappropriate?</p>
-      <p>
-        You can email
-        <a href="mailto:support@excluzeev.com">support@excluzeev.com</a>
-      </p>
-      <p>&nbsp;</p>
-      <p>9. Is the a minimum or maximum price I am required to use when charging on my channel or can I charge anything I want?</p>
-      <p>You cannot charge less than $1 and more than $10 per channel.</p>
-      <p>&nbsp;</p>
-      <p>10. Do you have to Subscribe to watch the Previews?</p>
-      <p>All previews can be viewed without subscription.</p>
+      <p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">1. What is Excluzeev?</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">Excluzeev is a Streaming, Crowdfunding and Social Website/Mobile Application. Excluzeev offers content creators a platform to monetize their exclusive content.</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">&nbsp;</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">2. What is the payment gateway used on Excluzeev?</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">Stripe is used as the payment gateway on Excluzeev.</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">&nbsp;</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">3. What is the best way to contact Excluzeev?</span></p>
+<p class="m_-8867478834954173611p3"><span class="m_-8867478834954173611s3">Email us at&nbsp;<a href="mailto:support@excluzeev.com" target="_blank" rel="noopener"><span class="m_-8867478834954173611s4">support@excluzeev.com</span></a></span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">&nbsp;</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">4. Are there age restrictions for being a content creator on Excluzeev?</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">Content creators must be 18 years of age.</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">&nbsp;</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">5. How do I sign up as a user/content creator on Excluzeev?</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">To sign up as a content creator you need to go to the sign up link on Excluzeev, add your name, email and password. Confirm your email. You create a channel by selecting a content category and adding your Stripe account for subscriber payments.</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">&nbsp;</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">6. Can a subscriber unsubscribe at any time with no commitment?</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">Yes, you can unsubscribe at any time.</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">&nbsp;</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">7. Are people able to download or copy my content from the site?</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">No they cannot. Your content will be stream only.</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">&nbsp;</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">8. How do I report content I find offensive or inappropriate?</span></p>
+<p class="m_-8867478834954173611p3"><span class="m_-8867478834954173611s3">You can email&nbsp;<a href="mailto:support@excluzeev.com" target="_blank" rel="noopener"><span class="m_-8867478834954173611s4">support@excluzeev.com</span></a></span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">&nbsp;</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">9. Is the a minimum or maximum price I am required to use when charging on my channel or can I charge anything I want?</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">You cannot charge less than $1.99.</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">&nbsp;</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">10. Do you have to Subscribe to watch the Previews?</span></p>
+<p class="m_-8867478834954173611p2"><span class="m_-8867478834954173611s2">All previews can be viewed without subscription.</span></p>
       <!-- <h1 class="main_text">Licensing Agreement</h1> -->
     </div>
   </v-container>
