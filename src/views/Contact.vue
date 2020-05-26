980bde09a1c6e4b292b4149c05ea5fb566b53fed
kannan <kannan>
2020-05-26 08:43:11 +0000

    Added form validations in contact form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -10,30 +10,49 @@
 
 					</div>
 					<form>
+
+                      <div
+                      v-if="successMessage"
+                      class="success-message"
+                      v-text="successMessage"></div>
+                      <div
+                      v-if="errorMessage"
+                      class="error-message"
+                      v-text="errorMessage"
+                      />
+
+
 						<br>
 						<h5 style="text-align:center">Please Enter Your Details</h5>
 						<br>
 						<div class="form-row">
 							<div class="form-group col-md-6">
 								<label>First Name <span style="color:red ">*</span></label>
-								<input type="name" class="form-control" id="firstName" placeholder="Please enter first name" v-model="first_name">
+								<input type="name" :class="errors.first_name?'form-control input-border-error':'form-control'" id="firstName" placeholder="Please enter first name" v-model="first_name">
+								<div v-if="errors.first_name" class="label-error"><i class="fa fa-info-cirle"></i>&nbsp;{{errors.first_name}}</div>
+
 							</div>
 							<div class="form-group col-md-6">
 								<label>Last Name <span style="color:red">*</span></label>
-								<input type="name" class="form-control" id="lastName" placeholder="Please enter last name" v-model="last_name">
+								<input type="name" :class="errors.last_name?'form-control input-border-error':'form-control'" id="lastName" placeholder="Please enter last name" v-model="last_name">
+								<div v-if="errors.last_name" class="label-error"><i class="fa fa-info-cirle"></i>&nbsp;{{errors.last_name}}</div>
+
 							</div>
 
 						</div>
 						<div class="form-row">
 							<div class="form-group col-md-12">
 								<label>Email <span style="color:red">*</span></label>
-								<input type="email" class="form-control" id="email" placeholder="Please enter email address" v-model="email">
+								<input type="email" :class="errors.email?'form-control input-border-error':'form-control'" id="email" placeholder="Please enter email address" v-model="email">
+								<div v-if="errors.email" class="label-error"><i class="fa fa-info-cirle"></i>&nbsp;{{errors.email}}</div>
+
 							</div>
 						</div>
 						<div class="form-row">
 							<div class="form-group col-md-12">
 								<label>Message <span style="color:red"></span></label>
-								<textarea v-model="message" class="form-control" id="description" placeholder="Message" rows="5"></textarea>
+								<textarea v-model="message" :class="errors.message?'form-control input-border-error':'form-control'" id="description" placeholder="Message" rows="5"></textarea>
+								<div v-if="errors.message" class="label-error"><i class="fa fa-info-cirle"></i>&nbsp;{{errors.message}}</div>
 
 							</div>
 
@@ -43,7 +62,14 @@
 
 							<div class="form-group col-md-12 text-right">
 								&nbsp;&nbsp;
-								<button type="button" class="btn btn-windowSignUp btn-create" @click="sendSupportMail">Submit</button>
+								<button :disabled="isMailProcessing" type="button" class="btn btn-windowSignUp btn-create" @click="sendSupportMail">
+									   <template v-if="isMailProcessing">
+										  <i style="font-size:20px;" class="fa fa-circle-o-notch fa-spin"></i>Processing
+									  </template>
+									  <template v-else>
+										  Submit
+									  </template>
+								</button>
 							</div>
 						</div>
 					</form>
@@ -65,7 +91,10 @@ export default {
 	  	first_name:"",
 	  	last_name:"",
 	  	email:"",
-		message:""
+		message:"",
+		successMessage:null,
+		errorMessage:null,
+		isMailProcessing:false,
     };
   },
   created() {
@@ -84,10 +113,44 @@ export default {
 		this.last_name=""
 		this.email=""
 		this.message=""
-
+		this.successMessage=null
+		this.errorMessage=null
 	},
 	sendSupportMail() {
 
+		this.errors=[]
+  		console.log("Send email started");
+  		//Validation
+
+		let isError=false
+		if(this.first_name == ""){
+			this.errors['first_name']="This field is required";
+			isError=true
+		}
+
+		if(this.last_name == ""){
+			this.errors['last_name']="This field is required";
+			isError=true
+		}
+
+		if(this.email == ""){
+			this.errors['email']="This field is required";
+			isError=true
+		}
+
+		if(this.message == ""){
+			this.errors['message']="This field is required";
+			isError=true
+		}
+
+		if(isError){
+			this.isMailProcessing=false
+			this.$forceUpdate()
+			return;
+		}
+
+
+  		this.isMailProcessing=true
 
 
         try{
@@ -101,14 +164,36 @@ export default {
             this.$store.dispatch("sendEmail",queryParams).then((resp) => {
 
                 console.log("Signout successful",resp)
+				this.isMailProcessing=false
+
+				this.successMessage="Thanks, Successfully sent email to suppot team."
+
+				setTimeout(()=>{
+					this.successMessage=null
+					this.reset_form()
+				},3000)
 
             }).catch((error,data)=>{
             	 console.log("Error: ",error)
+				this.isMailProcessing=false
+
+				this.errorMessage="Sorry, unable to send email. Something went wrong !!!"
+
+				setTimeout(()=>{
+					this.errorMessage=null
+
+				},3000)
+
 			})
 
 
           }catch(err){
             console.log("Error: ",err)
+			this.errorMessage="Sorry, unable to send email. Internal error !!!"
+
+				setTimeout(()=>{
+					this.errorMessage=null
+				},3000)
           }
 
 
@@ -140,4 +225,36 @@ export default {
 			border-radius: 5px 5px 0 0;
 			border:1px solid #29abe2;
 		}
+
+
+    .success-message, .error-message{
+        /*padding: 5px;*/
+        text-align: center;
+        font-size: 16px;
+        /*border: 1px solid #f9f9f9;*/
+        /*margin-bottom: 5px;*/
+    }
+
+    .success-message{
+        color:green;
+    }
+    .error-message{
+
+        color:red;
+
+    }
+    .input-error{
+
+        font-size: 1.1rem;
+        padding: 5px;
+        color: red;
+
+    }
+    .input-border-error{
+        border-color: red !important;
+    }
+    .label-error{
+        color: red!important;
+        font: 16px lato-regular!important;
+    }
 </style>
